15dca0a5ee5c7d6697cbf33a5131b5f16db11e71
adrian kocifaj <adrian.kocifaj@gmail.com>
2026-04-26 21:03:16 +0200

    edit employee entity

--- src/CompanyStructure.Domain/Entities/Employee.cs
+++ src/CompanyStructure.Domain/Entities/Employee.cs
@@ -4,15 +4,15 @@
     {
         public Guid Id { get; set; }
 
-        public string Title { get; set; } = string.Empty;
+        public string? Title { get; set; }
 
         public string FirstName { get; set; } = string.Empty;
 
         public string LastName { get; set; } = string.Empty;
 
-        public string Phone { get; set; } = string.Empty;
+        public string? Phone { get; set; }
 
-        public string Email { get; set; } = string.Empty;
+        public string? Email { get; set; }
 
         public Guid CompanyId { get; set; }
 
